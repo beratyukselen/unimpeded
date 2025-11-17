c36be4c0b8949fd661726a1f9b78e77300470d99
beratyukselen <beratyukselen@gmail.com>
2025-11-17 17:29:19 +0300

    Inıtial Commit

---
+++ unimpeded/unimpeded/unimpededApp.swift
@@ -0,0 +1,17 @@
+//
+//  unimpededApp.swift
+//  unimpeded
+//
+//  Created by Berat Yükselen on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct unimpededApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
